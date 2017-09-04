31b96b3dbe1d9398fb9704b37865d6477df6046a
Roman Volkov <parrker9@gmail.com>
2017-09-04 13:04:06 +0800

    Fix the issue with PhantomJS not rendering React without a polyfill

--- fetchtoken.js
+++ fetchtoken.js
@@ -1,7 +1,25 @@
-
 var fs = require('fs');
 var system = require('system');
 var page = require('webpage').create();
+
+page.onInitialized = function() {
+    if (page.injectJs('core.js')) {
+        console.log('Polyfills loaded');
+    }
+}
+
+page.onError = function(msg, trace) {
+    var msgStack = ['ERROR: ' + msg];
+
+    if (trace && trace.length) {
+        msgStack.push('TRACE:');
+        trace.forEach(function(t) {
+            msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+        });
+    }
+
+    console.error(msgStack.join('\n'));
+}
 
 function sleep(ms) {
     var unixtime_ms = new Date().getTime();
@@ -11,7 +29,7 @@ function sleep(ms) {
 function getTokens(symbol) {
     url = 'https://finance.yahoo.com/quote/' + symbol + '/history?p=' + symbol;
     console.log('Fetch url', url);
-    page.settings.userAgent = Math.random().toString(36).substring(7);
+    page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36';
     page.open(url, function(status) {
         console.log('status:', status);
         var title = page.evaluate(function() {
@@ -19,22 +37,23 @@ function getTokens(symbol) {
         });
         console.log('title:', title);
 
-        page.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', function() {
-            sleep(10000); // need time to render the page
-            var href = page.evaluate(function() {
-                var anchor = $('a').filter(function(index) { return $(this).text() === "Download Data"; }).closest('a');
-                return anchor.attr('href');
-            });
-            console.log('csv url:', href);
-            console.log('crumb:', href.split('=').slice(-1).pop());
-            for (var x in phantom.cookies) {
-                var cookie = phantom.cookies[x];
-                if (cookie.name === 'B') {
-                    console.log('cookie: B=' + cookie.value)
-                }
-            }
-            phantom.exit();
+        sleep(10000); // need time to render the page
+
+        var href = page.evaluate(function() {
+            return document.querySelector('span a').getAttribute('href');
         });
+
+        console.log('csv url:', href);
+        console.log('crumb:', href.split('=').slice(-1).pop());
+
+        for (var x in phantom.cookies) {
+            var cookie = phantom.cookies[x];
+            if (cookie.name === 'B') {
+                console.log('cookie: B=' + cookie.value)
+            }
+        }
+
+        phantom.exit();
     });
 }
 
